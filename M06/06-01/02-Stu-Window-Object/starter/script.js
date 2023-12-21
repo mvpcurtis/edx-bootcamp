deffed3bd556030d9aad2cd49bca6845b5fffd2a
mvpcurtis <curtis.bowen@gmx.com>
2023-12-21 10:55:58 +0000

    inital commit

---
+++ M06/06-01/02-Stu-Window-Object/starter/script.js
@@ -0,0 +1,15 @@
+// TODO: What will the following line of code log?
+console.log(window);
+// lists information about the window
+
+// TODO: What will the following line of code log?
+console.log(window.document);
+// lists information about the document
+
+// TODO: What will the following line of code log?
+console.log(document.documentElement);
+// lists information about the html element
+
+// TODO: What will the following line of code log?
+console.log(document.head);
+// lists the document head
---
+++ M06/06-02/06-Stu-Keyboard-Events/starter/assets/js/script.js
@@ -0,0 +1,13 @@
+function keydownAction(event) {
+  // TODO: Complete keydown function
+  let key = document.querySelector('#key');
+  function
+}
+
+function keyupAction() {
+  document.querySelector("#status").innerHTML = "KEYUP Event";
+}
+
+document.addEventListener("keyup", keyupAction);
+// TODO: Add Event Listener for "keydown" event
+
